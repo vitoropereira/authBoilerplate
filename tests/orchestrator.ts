e1a70b6ee43090460970b7c5ba4a964be92af3a1
VITOR PEREIRA <VOP1234@HOTMAIL.COM>
2022-12-02 17:18:31 -0300

    :white_check_mark: finished auth tests

--- tests/orchestrator.ts
+++ tests/orchestrator.ts
@@ -1,5 +1,11 @@
-import retry from "async-retry";
 import { prisma } from "../src/lib/prisma";
+import jwt from "jsonwebtoken";
+
+export interface CreateUsersProps {
+  username: string;
+  email: string;
+  password: string;
+}
 
 export const webserverUrl = `http://${process.env.WEBSERVER_HOST}:${process.env.WEBSERVER_PORT}`;
 
@@ -12,3 +18,37 @@ export function clearDB() {
     await prisma.$disconnect();
   });
 }
+
+export async function createUsers({
+  username,
+  email,
+  password,
+}: CreateUsersProps) {
+  const response = await fetch(`${webserverUrl}/api/v1/users/create-user`, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+    }),
+  });
+
+  const { user } = await response.json();
+  return user;
+}
+
+export const isTokenExpired = (token: string): boolean => {
+  try {
+    const { exp } = jwt.decode(token) as {
+      exp: number;
+    };
+    const expirationDateTimeInSeconds = exp * 1000;
+
+    return Date.now() >= expirationDateTimeInSeconds;
+  } catch {
+    return true;
+  }
+};
